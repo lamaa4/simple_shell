dce144a3829c56ec614dd96b57fbdd0824674686
lamaa4 <44681715+lamaa4@users.noreply.github.com>
2023-08-16 09:57:22 +0200

    Create main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+extern char **environ;
+
+#endif
