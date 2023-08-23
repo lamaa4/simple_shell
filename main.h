b6eabf563b11b0e8a03015a1e3405f968ebb7fd1
lamaa4 <44681715+lamaa4@users.noreply.github.com>
2023-08-23 19:01:49 +0200

    Update main.h

--- main.h
+++ main.h
@@ -1,29 +1,37 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
+#include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
+#include <sys/wait.h>
+
 extern char **environ;
 
-/* Main */
-ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
-
+/* Main functions */
+ssize_t _getline(char **lineptr, size_t *len, int fd);
 
 /* String functions */
 int _strlen(const char *s);
 char *_strcat(char *dest, const char *src);
-int _strcmp(char *s1, char *s2);
+int _strcmp(const char *s1, char *s2);
 char *_strdup(const char *);
-char *_strndup(const char *src, size_t start_pos, size_t n);
+char *_strcpy(char *dest, const char *src);
 
 /* Tokenize functions */
 char *token(const char *str, const char *charset);
 char *_strtok(char *str, const char *delim);
 int is_delim(char c, const char *delim);
+int count_tokens(char *str, char *delim);
+int token_len(char *str, char *delim);
+
+/* Execute functions */
+int execute_builtin(const char *command, char *const args[]);
+char *_strchr(const char *str, int character);
+int cmd_execute(const char *filename, char *const argv[]);
 
 #endif
