44b33be7a619cfd2b02b002a9d771176c3588a55
lamaa4 <44681715+lamaa4@users.noreply.github.com>
2023-08-24 01:31:23 +0200

    Update execute.c

--- execute.c
+++ execute.c
@@ -65,11 +65,11 @@ int execute_builtin(const char *cmd, char *const args[])
   
 int cmd_execute(const char *filename, char *const argv[]) 
 {
-        if (_strchr(filename, '/') != NULL)
+        /*if (_strchr(filename, '/') != NULL)
         {
                 return (execve(filename, argv, NULL));
         }
-        else
+        else*/
         {
                 char *path_env;
                 path_env = getenv("PATH");
