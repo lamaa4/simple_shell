a44eea54315c1706c34b57647ce030605d37b763
lamaa4 <44681715+lamaa4@users.noreply.github.com>
2023-08-24 11:44:55 +0200

    Update main.c

--- main.c
+++ main.c
@@ -46,6 +46,7 @@ if (non_interactive)
 ssize_t read_size;
 
 read_size = _getline(&line, &line_size, fd);
+write(STDOUT_FILENO, "$ ", 2);
 
 if (read_size == -1)
 {
@@ -61,13 +62,13 @@ else
 {
 ssize_t read_size;
 
-write(STDOUT_FILENO, "$ ", 2);
+//write(STDOUT_FILENO, "$ ", 2);
 
 read_size = _getline(&line, &line_size, STDIN_FILENO);
 
 if (read_size == -1)
 {
-write(STDOUT_FILENO, "\n", 1);
+//write(STDOUT_FILENO, "\n", 1);
 break;
 }
 
