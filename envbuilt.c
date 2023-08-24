abeab76b3e1a80515eba705029d435d8e11e3ade
lamaa4 <44681715+lamaa4@users.noreply.github.com>
2023-08-24 02:00:07 +0200

    Create envbuilt.c

---
+++ envbuilt.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+int print_env(const char *cmd);
+
+/**
+ * print_env - print env
+ * @cmd: input
+ */
+
+int print_env(const char *cmd)
+{
+	char **env = environ;
+	int i;
+	
+	if (!env)
+		return (-1);
+	        
+	for (i = 0; env[i] != NULL; i++)
+	{
+        printf("%s\n", env[i]);
+        fflush(stdout);
+	}
+        return (0);
+}
